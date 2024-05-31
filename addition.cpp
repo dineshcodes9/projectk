e560724f1727e46845336e3747ba10bdccad3939
dineshcodes9 <dineshstudent2021@gmail.com>
2024-05-31 12:32:27 +0530

    addition file is created with code

--- addition.cpp
+++ addition.cpp
@@ -1,6 +1,10 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main ()
 {
-    cout<<"Hello World";
+    int year;
+    cout<<"Enter any year"<<endl;
+    cin>> year;
+    cout<<"year is "<<year<<endl;
+    return 0;
 }
